c36799529370ad3fed51923c5c076e3ccec62cc0
Mahak Kanwar <mahakkanwar0@gmail.com>
2026-05-03 10:51:24 +0530

    Removed secret

--- db.py
+++ db.py
@@ -1,17 +1,21 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base,sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL="mysql+pymysql://2aB6tBZCH7kTuw3.root:Bhel4fhL60vBzsUa@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test?ssl_ca=C:\\tidb\\isrgrootx1.pem&ssl_verify_cert=true&ssl_verify_identity=true"
+load_dotenv()
 
-engine=create_engine(
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
     connect_args={
-        "ssl":{
-            "ssl":True
+        "ssl": {
+            "ssl": True
         }
     }
 )
 
-SessionLocal=sessionmaker(bind=engine)
-Base=declarative_base()
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
